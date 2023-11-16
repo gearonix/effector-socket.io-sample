3a649acbc2077ec668763997d8923b03a11d0af7
Gearonix <egor.uzhanin@gmail.com>
2023-11-16 08:12:28 +0300

    test: wrote test for scope, validate-zod-schema

---
+++ src/scope.test.ts
@@ -0,0 +1,90 @@
+import { createEvent }       from 'effector'
+import { createStore }       from 'effector'
+import { sample }            from 'effector'
+import { createGate }        from 'effector-react'
+
+import { scope }             from './scope'
+import { ConnectedInstance } from './shared/types'
+
+const actualEffector = jest.requireActual('effector')
+const actualEffectorReact = jest.requireActual('effector-react')
+
+jest.mock('effector', () => ({
+  createEvent: jest.fn(() => actualEffector.createEvent()),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  createStore: jest.fn((...args: any[]) => actualEffector.createStore(args)),
+  sample: jest.fn()
+}))
+
+jest.mock('effector-react', () => ({
+  createGate: jest.fn(() => actualEffectorReact.createGate())
+}))
+
+describe('scope', () => {
+  let parent: ConnectedInstance<Record<string, string>>
+
+  beforeEach(() => {
+    parent = {
+      $instance: {},
+      Gate: jest.fn(),
+      publisher: jest.fn(),
+      subscribe: jest.fn()
+    } as unknown as ConnectedInstance<Record<string, string>>
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should call createGate, createStore, createEvent inside of function', () => {
+    scope(parent)
+
+    expect(createGate).toHaveBeenCalled()
+    expect(createStore).toHaveBeenCalled()
+    expect(createEvent).toHaveBeenCalled()
+  })
+
+  it('should create a subscription with the ChildGate', () => {
+    const { subscribe } = scope(parent)
+
+    const mockSubscribe = parent.subscribe as jest.MockedFunction<
+      typeof parent.subscribe
+    >
+
+    subscribe('method')
+
+    expect(mockSubscribe).toHaveBeenCalledWith('method', {
+      OverrideGate: expect.anything()
+    })
+  })
+
+  it('should create a publisher with the ChildGate', () => {
+    const { publisher } = scope(parent)
+
+    const mockPublisher = parent.publisher as jest.MockedFunction<
+      typeof parent.publisher
+    >
+
+    publisher('method')
+
+    expect(mockPublisher).toHaveBeenCalledWith('method', {
+      OverrideGate: expect.anything()
+    })
+  })
+
+  it('should unsubscribe when ChildGate is closed', () => {
+    const { Gate } = scope(parent)
+
+    Gate.close()
+
+    expect(sample).toHaveBeenCalledWith({
+      clock: Gate.close,
+      fn: expect.any(Function),
+      source: {
+        events: expect.any(Object),
+        instance: expect.any(Object)
+      },
+      target: expect.any(Function)
+    })
+  })
+})
---
+++ src/shared/lib/validate-zod-schema.test.ts
@@ -0,0 +1,52 @@
+import { z }                 from 'zod'
+
+import { validateZodSchema } from './validate-zod-schema'
+
+describe('validate-zod-schema', () => {
+  const successSchema = z.object({
+    age: z.number(),
+    name: z.string()
+  })
+
+  const successPayload = {
+    age: 25,
+    name: 'John Doe'
+  }
+
+  const failureSchema = z.object({
+    email: z.string().email(),
+    username: z.string()
+  })
+
+  const failurePayload = {
+    email: 'invalid_email',
+    username: 'john_doe'
+  }
+
+  const failureMethod = 'exampleMethod'
+
+  it('should validate successfully and return the payload for a valid schema', () => {
+    const result = validateZodSchema(
+      successSchema,
+      successPayload,
+      'successMethod'
+    )
+    expect(result).toEqual(successPayload)
+  })
+
+  it('should log an error and return undefined for an invalid schema', () => {
+    console.error = jest.fn()
+    console.info = jest.fn()
+
+    const result = validateZodSchema(
+      failureSchema,
+      failurePayload,
+      failureMethod
+    )
+
+    expect(console.error).toHaveBeenCalled()
+    expect(console.info).toHaveBeenCalled()
+
+    expect(result).toBeUndefined()
+  })
+})
